a73aee4a833c825a08fad88828ecc95bd166ac72
Charlie Kapsiak <kapsi002@umn.edu>
2023-02-14 14:18:36 -0600

    Fixing issues with jet histograms and changing matching output

--- match_algos.py
+++ match_algos.py
@@ -12,7 +12,7 @@ def globalMatcher(jets, particles, dr_match=0.3, combo_count=8, require_all_matc
     njets = len(jets_to_use)
     loss = None 
     best_match = None
-    for perm in itertools.permutations(range(njets), mum_to_test):
+    for perm in itertools.permutations(range(njets), num_to_test):
         #this_perm = [jets[x] for x in perm]
         closeness = [DR(get4(particles[i]),get4(jets[perm[i]]))**2 for i in range(num_to_test)]
         if require_all_matches and any(x > dr_match for x in closeness):
@@ -21,7 +21,7 @@ def globalMatcher(jets, particles, dr_match=0.3, combo_count=8, require_all_matc
 #       print("Permutation {} has loss {}".format(perm, new_loss))
         if loss is None  or new_loss < loss:
             loss = new_loss
-            best_match = perm
+            best_match = perm[:num_to_test]
     return best_match
 
 def orderedMatcher(jets, particles, dr_match=0.3):
@@ -31,18 +31,19 @@ def orderedMatcher(jets, particles, dr_match=0.3):
         return self.DeltaR(other)
     matched = []
     remaining_idxs = set(range(len(jets)))
-
+    used_idxs = set()
     for i,p in enumerate(particles):
-        if not remaining_idxs:
-            matched.append(None)
-            continue
-        smallest = min(remaining_idxs, key=lambda x : DR(get4(p),get4(jets.__getitem__(x))))
-        if DR(get4(p),get4(jets.__getitem__(smallest))) > dr_match:
-            matched.append(None)
-            continue
-        remaining_idxs.remove(smallest)
-        matched.append(smallest)
-
+        diff=lambda x : DR(get4(p),get4(jets.__getitem__(x)))
+        if remaining_idxs: smallest = min(remaining_idxs, key=diff)
+        if remaining_idxs and diff(smallest) < dr_match:
+            remaining_idxs.remove(smallest)
+            used_idxs.add(smallest)
+            matched.append((smallest,None))
+        elif used_idxs:
+            smallest = min(used_idxs, key=diff)
+            if diff(smallest) < dr_match: matched.append((None,smallest))
+            else: matched.append((None,None))
+        else: matched.append((None,None))
     return matched
 
 def priorityOrderedMatcher(jets, particles, dr_match=0.3):
