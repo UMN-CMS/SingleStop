d7d12350a3698021c2151f82e5acf109e5d5d079
Shardul Rao <shardulr2931@gmail.com>
2023-02-24 21:29:14 -0600

    Continuing to Test Stack Plots

--- stackPlotter.py
+++ stackPlotter.py
@@ -4,21 +4,16 @@ path = 'root-test-files'
 files = ['QCD.root', 'TT.root', 'signal_1500_600.root']
 colors = [2, 3, 4]
 h_stack = ROOT.THStack()
+
+ROOT.gStyle.SetPalette(ROOT.kOcean)
+
 for i, f in enumerate(files):
 	filepath = '{}/{}'.format(path, f)
 	newfile = ROOT.TFile.Open(filepath, "READ")
 	h_pT1Gen = newfile.Get('pT1Gen')
-	h_pT1Gen.SetFillColor(colors[i])
-	h_pT1Gen.SetMarkerColor(colors[i])
-	h_pT1Gen.SetLineColor(colors[i])
-	h_stack.Add(h_pT1Gen)		
-	
+	#ROOT.gStyle.SetPalette(3)	
+	h_stack.Add(h_pT1Gen)	
+
 c1 = ROOT.TCanvas()
-h_stack.Draw()
+h_stack.Draw('HIST')
 c1.SaveAs('plots/test-stack.png')
-
-
-
-
- 
-
