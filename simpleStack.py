ca145d4e053835dee58ad25956dafcbc762c9e1f
Shardul Rao <shardulr2931@gmail.com>
2023-02-28 13:58:44 -0600

    Stack Plotter as of Feb. 28 Meeting

--- simpleStack.py
+++ simpleStack.py
@@ -4,13 +4,13 @@ path = 'root-test-files'
 files = ['QCD.root', 'TT.root', 'signal_1500_600.root']
 colors = [2, 3, 4]
 h_stack = ROOT.THStack()
-
+c1 = ROOT.TCanvas()
+#ROOT.gStyle.SetPalette(ROOT.kOcean)
 for i, f in enumerate(files):
 	file_path = '{}/{}'.format(path, f)
 	new_file = ROOT.TFile.Open(file_path, "READ")
 	h_pT1Gen = new_file.Get('pT1Gen')
+	#h_pT1Gen.Draw('HIST')
 	h_stack.Add(h_pT1Gen)
-
-c1 = ROOT.TCanvas()
-h_stack.Draw('HIST PFC')
-c1.SaveAs('plots/simple-test-stack.png')
+h_stack.Draw('HIST')
+c1.SaveAs('plots/pT1Gen-stack.png')
