a21ad950a633c8877b4b390ef3287405689b9d04
Shardul Rao <shardulr2931@gmail.com>
2023-03-02 16:31:09 -0600

    Working stack plotter w/o proper labeling

--- stackPlotter.py
+++ stackPlotter.py
@@ -1,5 +1,5 @@
 import os, argparse, math, ROOT
-
+ROOT.gROOT.SetBatch(True)
 path = 'root-test-files'
 files = ['QCD.root', 'TT.root', 'signal_1500_600.root']
 colors = [2, 3, 4]
@@ -12,15 +12,19 @@ param_list = [key.GetName() for key in file1.GetListOfKeys()]
 ROOT.TFile.Close(file1)
 
 for param in param_list:
-	h_stack = ROOT.THStack()
+	h_stack = ROOT.THStack("stack", param)
+	print(param)
 	for i, f in enumerate(files):
 		filepath = '{}/{}'.format(path, f)
 		newfile = ROOT.TFile.Open(filepath, "READ")	
 		h_new = newfile.Get(param)
-		ROOT.TFile.Close(newfile)
+		if not h_new:
+			break
+		#ROOT.TFile.Close(newfile)
 		#ROOT.gStyle.SetPalette(3)	
 		h_stack.Add(h_new)	
-
+	if not h_new or not h_new.InheritsFrom('TH1'):
+		continue
 	c1 = ROOT.TCanvas()
 	h_stack.Draw('HIST')
 	c1.SaveAs('plots/{}.png'.format(param))
