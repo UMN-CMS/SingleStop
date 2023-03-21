e0464ff03252f902babc60c651ae1f6a1fddd15d
Shardul Rao <shardulr2931@gmail.com>
2023-03-21 17:14:13 -0500

    Test

--- stackPlotter.py
+++ stackPlotter.py
@@ -1,15 +1,16 @@
 import os, argparse, math, ROOT
 ROOT.gROOT.SetBatch(True)
 path = 'root-test-files'
-files = ['QCD.root', 'TT.root', 'signal_1500_600.root']
+files = ['QCD.root', 'TT.root']
 colors = [2, 3, 4]
 
 
 ROOT.gStyle.SetPalette(ROOT.kOcean)
 
-file1 = ROOT.TFile.Open('root-test-files/signal_1500_600.root', "READ")
+file1 = ROOT.TFile.Open('{}/{}'.format(path, files[0]), "READ")
 param_list = [key.GetName() for key in file1.GetListOfKeys()]
 ROOT.TFile.Close(file1)
+param_list = ['HT']
 
 for param in param_list:
 	h_stack = ROOT.THStack(param, "{};{};Events".format(param, param))
@@ -27,5 +28,7 @@ for param in param_list:
 		continue
 	c1 = ROOT.TCanvas()
 	ROOT.gPad.SetLogy()
+	ROOT.THStack.SetMinimum(h_stack, 1)	
+	ROOT.THStack.SetMaximum(h_stack, 10**8)
 	h_stack.Draw('HIST')
 	c1.SaveAs('plots/{}.png'.format(param))
