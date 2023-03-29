fc5c776e9036cc9f6b592e6292abca346c9c057c
Shardul Rao <shardulr2931@gmail.com>
2023-03-28 23:46:45 -0500

    Working stack plotter with scaled axes

--- stackPlotter.py
+++ stackPlotter.py
@@ -1,7 +1,7 @@
 import os, argparse, math, ROOT
 ROOT.gROOT.SetBatch(True)
 path = 'root-test-files'
-files = ['QCD.root', 'TT.root']
+files = ['signal_1500_600.root', 'QCD.root', 'TT.root']
 colors = [2, 3, 4]
 
 
@@ -10,25 +10,21 @@ ROOT.gStyle.SetPalette(ROOT.kOcean)
 file1 = ROOT.TFile.Open('{}/{}'.format(path, files[0]), "READ")
 param_list = [key.GetName() for key in file1.GetListOfKeys()]
 ROOT.TFile.Close(file1)
-param_list = ['HT']
+files = {fname: ROOT.TFile.Open('{}/{}'.format(path, fname), "READ") for fname in files}
 
 for param in param_list:
 	h_stack = ROOT.THStack(param, "{};{};Events".format(param, param))
 	print(param)
 	for i, f in enumerate(files):
-		filepath = '{}/{}'.format(path, f)
-		newfile = ROOT.TFile.Open(filepath, "READ")	
+		newfile = files[f]	
 		h_new = newfile.Get(param)
-		if not h_new:
-			break
-		#ROOT.TFile.Close(newfile)
-		#ROOT.gStyle.SetPalette(3)	
-		h_stack.Add(h_new)	
-	if not h_new or not h_new.InheritsFrom('TH1'):
-		continue
+		if h_new == None or not h_new.InheritsFrom('TH1F'):
+			continue	
+		h_stack.Add(h_new)
 	c1 = ROOT.TCanvas()
-	ROOT.gPad.SetLogy()
-	ROOT.THStack.SetMinimum(h_stack, 1)	
-	ROOT.THStack.SetMaximum(h_stack, 10**8)
 	h_stack.Draw('HIST')
+	ROOT.gPad.SetLogy()
+	h_stack.SetMinimum(1)	
+	h_stack.SetMaximum(10**8)
+	#h_stack.Draw('HIST')
 	c1.SaveAs('plots/{}.png'.format(param))
