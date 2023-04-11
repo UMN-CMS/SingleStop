ca00f84af6d75256a8a0ae68c608416137eb9ac6
Shardul Rao <shardulr2931@gmail.com>
2023-04-11 12:56:01 -0500

    Plotting 312 and 313 coupling signals

--- saveTwoCouplings.C
+++ saveTwoCouplings.C
@@ -6,6 +6,7 @@ void saveTwoCouplings(){
   int doTrigMassPlots = 0;
 
   string inputDirs[]= ["313", "312"];
+  string saveTo = "313_312";
 
   if (doPlots){
     vector<string> masses = {"2000_100"};//{"200_100","300_100","300_200","500_100","500_200","500_400","700_100","700_400","700_600","1000_100","1000_400","1000_900","1500_100","1500_600","1500_1400","2000_100","2000_900","2000_1900"};
@@ -19,7 +20,7 @@ void saveTwoCouplings(){
 			    TH1F* h = (TH1F*)key->ReadObj();
 			    TCanvas* c1 = new TCanvas();
 			    h->Draw();
-			    c1->SaveAs(Form("plots/%s/%s/%s.png",inputDir.c_str(),s.c_str(),h->GetName()));
+			    c1->SaveAs(Form("plots/%s/%s/%s.png",saveTo.c_str(),s.c_str(),h->GetName()));
 		    }
 	    }
     }
@@ -32,43 +33,43 @@ void saveTwoCouplings(){
     vector<string> compare = {"signal_1000_400","signal_1000_900","signal_1500_900","signal_2000_400","signal_2000_1900"};
     vector<string> plotNames = {"nbLoose","nbMedium","nbTight","HT","nJets","mAll","m4","m3","pT1","pT2","pT3","pT4","eta1","eta2","eta3","eta4","m3NoLead","m3NoLeadOrSub","dEta12","dPhi12","dR12","ntLoose","ntMedium","ntTight","MET","dPhiMET1","dPhiMET2","dPhiMET3","dPhiMET4","HTLHE","nQLHE","nGLHE","nJLHE","pT1Gen","pT2Gen","pT3GEn","pT4Gen","eta1Gen","eta2Gen","eta3Gen","eta4Gen","pTStop","pTStopPlus","pTStopMinus","pTChi","pTChiPlus","pTChiMinus","pTBStop","pTBStopPlus","pTBStopMinus","pTBChi1","pTBChi2","pTBChiPlus","pTBChiMinus","etaStop","etaStopPlus","etaStopMinus","etaChi","etaChiPlus","etaChiMinus","etaBStop","etaBStopPlus","etaBStopMinus","etaBChi","etaBChiPlus","etaBChiMinus","dEtaBChi","dPhiBChi","nJetsChiMerged","dRBB","dEtaBB","dPhiBB","passDijet","dEtaWJs","mWJs","dRChiMax","dRBChi"};
     //float yMax = 0;
-    for (string inputDir:inputDirs) {
-	    for(string n:plotNames){
-		    TCanvas* c1 = new TCanvas();
-		    THStack* stack = new THStack();
-		    TLegend* legend = new TLegend(0.68,0.89,0.89,0.65);
-		    string xTitle, yTitle;
-		    for(string s:compare){
-			    TFile* file = new TFile(Form("output/%s/%s.root",inputDir.c_str(),s.c_str()));
-			    //TDirectory* plots = file->GetDirectory("plots");
-			    TH1D* plot = (TH1D*)file->Get(n.c_str());
-			    //TH1D* plot = (TH1D*)plots->Get(n.c_str());
-			    stringstream plotTitle;
-			    //plotTitle << "(" << s.substr(0,s.find("_")) << ", " << s.substr(s.find("_") + 1) << ")";
-			    plotTitle << s;
-			    plot->SetTitle(plotTitle.str().c_str());
-			    plot->SetLineWidth(3);
-			    plot->SetMarkerSize(0);
-			    plot->Scale(1. / plot->Integral());
-			    legend->AddEntry(plot);
-			    stack->Add(plot);
-			    xTitle = plot->GetXaxis()->GetTitle();
-			    yTitle = plot->GetYaxis()->GetTitle();
-			    //plot->Draw("HIST PLC PMC SAME");
-			    //if (plot->GetMaximum() > yMax) yMax = plot->GetMaximum();
-		    } 
-		    stack->Draw("HIST PLC NOSTACK");
-		    stack->GetXaxis()->SetTitle(xTitle.c_str());
-		    //stack->GetYaxis()->SetTitle("Fraction of Events");
-		    stack->GetYaxis()->SetTitle("Events");
-		    //legend->SetHeader("( m_{#tilde{t}} , m_{#tilde{#chi_{1}^{#pm}}} )","C");
-		    //legend->SetTextSize(0.03);
-		    legend->Draw();
-		    //TLegendEntry *header = (TLegendEntry*)legend->GetListOfPrimitives()->First();
-		    //header->SetTextSize(0.05);
-		    gPad->SetLogy();
-		    c1->SaveAs(Form("plots/%s/overlaid/%s.pdf",inputDir.c_str(),n.c_str()));
-	    }
+    for(string n:plotNames){
+	    TCanvas* c1 = new TCanvas();
+	    THStack* stack = new THStack();
+	    TLegend* legend = new TLegend(0.68,0.89,0.89,0.65);
+	    string xTitle, yTitle;
+	    for(string s:compare){
+		for (string inputDir:inputDirs) {
+		    TFile* file = new TFile(Form("output/%s/%s.root",inputDir.c_str(),s.c_str()));
+		    //TDirectory* plots = file->GetDirectory("plots");
+		    TH1D* plot = (TH1D*)file->Get(n.c_str());
+		    //TH1D* plot = (TH1D*)plots->Get(n.c_str());
+		    stringstream plotTitle;
+		    //plotTitle << "(" << s.substr(0,s.find("_")) << ", " << s.substr(s.find("_") + 1) << ")";
+		    plotTitle << s;
+		    plot->SetTitle(plotTitle.str().c_str());
+		    plot->SetLineWidth(3);
+		    plot->SetMarkerSize(0);
+		    plot->Scale(1. / plot->Integral());
+		    legend->AddEntry(plot);
+		    stack->Add(plot);
+		    xTitle = plot->GetXaxis()->GetTitle();
+		    yTitle = plot->GetYaxis()->GetTitle();
+		    //plot->Draw("HIST PLC PMC SAME");
+		    //if (plot->GetMaximum() > yMax) yMax = plot->GetMaximum();
+	    	}
+	    } 
+	    stack->Draw("HIST PLC NOSTACK");
+	    stack->GetXaxis()->SetTitle(xTitle.c_str());
+	    //stack->GetYaxis()->SetTitle("Fraction of Events");
+	    stack->GetYaxis()->SetTitle("Events");
+	    //legend->SetHeader("( m_{#tilde{t}} , m_{#tilde{#chi_{1}^{#pm}}} )","C");
+	    //legend->SetTextSize(0.03);
+	    legend->Draw();
+	    //TLegendEntry *header = (TLegendEntry*)legend->GetListOfPrimitives()->First();
+	    //header->SetTextSize(0.05);
+	    gPad->SetLogy();
+	    c1->SaveAs(Form("plots/%s/overlaid/%s.pdf",saveTo.c_str(),n.c_str()));
     }
   }
 
@@ -80,11 +81,11 @@ void saveTwoCouplings(){
     //TH1F* trigEff = new TH1F("trigEff",";Trigger;Efficiency",9,0,9);
     THStack* stack = new THStack("trigEff",";;Efficiency");
     TLegend* legend = new TLegend(0.7,0.85,0.85,0.45);
-    for (string inputDir:inputDirs) {
-	    for (string s:compare){
-		    TH1F *plotAll = new TH1F("","",9,0,9);
-		    plotAll->SetTitle(s.c_str());
-		    for (int i = 0; i < plotNames.size(); i++){
+    for (string s:compare){
+	    TH1F *plotAll = new TH1F("","",9,0,9);
+	    plotAll->SetTitle(s.c_str());
+	    for (int i = 0; i < plotNames.size(); i++){
+		    for (string inputDir:inputDirs) {
 			    string n = plotNames[i];
 			    TFile* file = new TFile(Form("output/%s/hist_%s.root",inputDir.c_str(),s.c_str()));
 			    TDirectory* plots = file->GetDirectory("plots");
@@ -92,21 +93,21 @@ void saveTwoCouplings(){
 			    plotAll->SetBinContent(i+1,plot->GetMean());
 			    plotAll->SetLineWidth(2);
 		    }
-		    legend->AddEntry(plotAll);
-		    stack->Add(plotAll);
 	    }
-	    stack->Draw("HIST PLC NOSTACK");
-	    for(int i = 0; i < plotNames.size(); i++){
-		    stack->GetXaxis()->SetBinLabel(i+1,plotNames[i].erase(0,4).c_str());
-	    }
-	    stack->GetXaxis()->SetLabelSize(0.028);
-	    stack->GetXaxis()->LabelsOption("u");
-	    //gPad->SetLogy();
-	    legend->Draw();
-	    gPad->SetBottomMargin(0.3);
-	    c1->SaveAs(Form("plots/%s/overlaid/%s.png",inputDir.c_str(),"trigEff"));
+	    legend->AddEntry(plotAll);
+	    stack->Add(plotAll);
     }
-  }
+  stack->Draw("HIST PLC NOSTACK");
+  for(int i = 0; i < plotNames.size(); i++){
+	stack->GetXaxis()->SetBinLabel(i+1,plotNames[i].erase(0,4).c_str());
+  }  
+  stack->GetXaxis()->SetLabelSize(0.028);
+  stack->GetXaxis()->LabelsOption("u");
+  //gPad->SetLogy();
+  legend->Draw();
+  gPad->SetBottomMargin(0.3);
+  c1->SaveAs(Form("plots/%s/overlaid/%s.png",saveTo.c_str(),"trigEff"));
+}
 
   gStyle->SetPalette(kBlueRedYellow);
   if (doTrigMassPlots){
@@ -117,39 +118,39 @@ void saveTwoCouplings(){
     TCanvas* c1 = new TCanvas();
     TMultiGraph* mg = new TMultiGraph();
     TLegend* legend = new TLegend(0.55,0.60,0.85,0.35);
-    for (string inputDir:inputDirs) {
-	    for (int i = 0; i < plotNames.size(); i++){
-		    vector<double> eff, effError;
-		    for (string s:compare){
-			    string n = plotNames[i];
-			    TFile* file = new TFile(Form("output/%s/hist_%s.root",inputDir.c_str(),s.c_str()));
-			    TDirectory* plots = file->GetDirectory("plots");
-			    TH1F* plot = (TH1F*)plots->Get(n.c_str());
-			    eff.push_back(plot->GetMean());
-			    effError.push_back(sqrt(plot->GetMean()) / nTot);
-		    }
-		    TGraphErrors* gre = new TGraphErrors(eff.size(),&mstop[0],&eff[0],0,0);//&effError[0]);
-		    gre->SetTitle(plotNames[i].c_str());
-		    //gre->SetMarkerColor();
-		    gre->SetMarkerStyle(20);
-		    gre->SetMarkerSize(0.8);
-		    mg->Add(gre);
-		    legend->AddEntry(gre);
+    for (int i = 0; i < plotNames.size(); i++){
+	    vector<double> eff, effError;
+	    for (string s:compare){
+		for (string inputDir:inputDirs) {
+		    string n = plotNames[i];
+		    TFile* file = new TFile(Form("output/%s/hist_%s.root",inputDir.c_str(),s.c_str()));
+		    TDirectory* plots = file->GetDirectory("plots");
+		    TH1F* plot = (TH1F*)plots->Get(n.c_str());
+		    eff.push_back(plot->GetMean());
+		    effError.push_back(sqrt(plot->GetMean()) / nTot);
+		}
 	    }
-	    mg->Draw("ALP PMC PLC");
-	    mg->GetXaxis()->SetTitle("m_{#tilde{t}} [GeV]");
-	    mg->GetYaxis()->SetTitle("Trigger Efficiency");
-	    mg->SetMinimum();
-	    //for(int i = 0; i < plotNames.size(); i++){
-	    //  stack->GetXaxis()->SetBinLabel(i+1,plotNames[i].erase(0,4).c_str());
-	    //}
-	    //stack->GetXaxis()->SetLabelSize(0.028);
-	    //stack->GetXaxis()->LabelsOption("u");
-	    legend->Draw();
-	    //gPad->SetBottomMargin(0.3);
-	    c1->SaveAs(Form("plots/%s/overlaid/%s.png",inputDir.c_str(),"trigEffMass"));
-	    gPad->SetLogy();
-	    c1->SaveAs(Form("plots/%s/overlaid/%s.png",inputDir.c_str(),"trigEffMassLog"));
+	    TGraphErrors* gre = new TGraphErrors(eff.size(),&mstop[0],&eff[0],0,0);//&effError[0]);
+	    gre->SetTitle(plotNames[i].c_str());
+	    //gre->SetMarkerColor();
+	    gre->SetMarkerStyle(20);
+	    gre->SetMarkerSize(0.8);
+	    mg->Add(gre);
+	    legend->AddEntry(gre);
     }
-  }
+    mg->Draw("ALP PMC PLC");
+    mg->GetXaxis()->SetTitle("m_{#tilde{t}} [GeV]");
+    mg->GetYaxis()->SetTitle("Trigger Efficiency");
+    mg->SetMinimum();
+    //for(int i = 0; i < plotNames.size(); i++){
+    //  stack->GetXaxis()->SetBinLabel(i+1,plotNames[i].erase(0,4).c_str());
+    //}
+    //stack->GetXaxis()->SetLabelSize(0.028);
+    //stack->GetXaxis()->LabelsOption("u");
+    legend->Draw();
+    //gPad->SetBottomMargin(0.3);
+    c1->SaveAs(Form("plots/%s/overlaid/%s.png",saveTo.c_str(),"trigEffMass"));
+    gPad->SetLogy();
+    c1->SaveAs(Form("plots/%s/overlaid/%s.png",saveTo.c_str(),"trigEffMassLog"));
+    }
 }
