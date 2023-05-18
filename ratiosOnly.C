c3de6092da126000beb34a4e96e0fefbf92f7859
Shardul Rao <shardulr2931@gmail.com>
2023-05-18 11:26:38 -0500

    Modified stack plotter to generate cutflows for various b-tag cuts

--- ratiosOnly.C
+++ ratiosOnly.C
@@ -92,7 +92,7 @@ void ratiosOnly(){
 	    //ratio->SetH2DrawOpt("HIST E0");
 	    //ratio->Draw();
 	    //ratio->GetLowerRefYaxis()->SetRangeUser(0.0, 2.0);
-	    stack->Draw("HIST PLC NOSTACK");
+	    stack->Draw("PLC NOSTACK");
 	    stack->GetXaxis()->SetTitle(xTitle.c_str());
 	    stack->GetYaxis()->SetTitle("Ratio of Events");
 	    //stack->GetYaxis()->SetTitle("Events");
--- stackPlotter.py
+++ stackPlotter.py
@@ -1,13 +1,14 @@
-import os, argparse, math, ROOT
+import ROOT
 ROOT.gROOT.SetBatch(True)
-signalPath = 'output/313'
+bTags = '021'
+signalPath = 'output/313/{}'.format(bTags)
 signalFiles = ['signal_2000_1900.root', 'signal_1500_900.root', 'signal_1000_400.root']
 colors = [2, 3, 4]
-backgroundPath = 'QCD2018/output'
+backgroundPath = 'QCD2018_{}/output'.format(bTags)
 backgroundFile = 'QCD2018.root'
 
 
-ROOT.gStyle.SetPalette(ROOT.kBlueRedYellow)
+ROOT.gStyle.SetPalette(ROOT.kSouthWest)
 
 file1 = ROOT.TFile.Open('{}/{}'.format(signalPath, signalFiles[0]), "READ")
 #file1 = ROOT.TFile.Open('output/313/signal_1500_600.root', "READ")
@@ -15,14 +16,15 @@ files = {fname: ROOT.TFile.Open('{}/{}'.format(signalPath, fname), "READ") for f
 files.update( {backgroundFile: ROOT.TFile.Open('{}/{}'.format(backgroundPath, backgroundFile), "READ") } )
 
 file1.cd("plots")
-param_list = [key.GetName() for key in ROOT.gDirectory.GetListOfKeys()]
-print(param_list)
+#param_list = [key.GetName() for key in ROOT.gDirectory.GetListOfKeys()]
+#print(param_list)
+param_list = ['cutflow']
 ROOT.TFile.Close(file1)
 
 for param in param_list:
 	h_stack = ROOT.THStack(param, "{};{};Events".format(param, param))
 	legend = ROOT.TLegend(0.68, 0.89, 0.89, 0.65)
-	print(param)
+	#print(param)
 	for i, f in enumerate(files):
 		newfile = files[f]
 		if f != "QCD2018.root": 
@@ -31,9 +33,12 @@ for param in param_list:
 		else:
 			h_new = newfile.Get(param)
 		if h_new == None or not (h_new.InheritsFrom('TH1F') or h_new.InheritsFrom('TH1D')): continue
-		if param == 'cutflow': h_new.Scale(1 / h_new.GetMaximum())	
+		if param == 'cutflow': 
+			h_new.Scale(1 / h_new.GetMaximum())
+			print(h_new.GetBinContent(6), h_new.GetBinContent(7))
+			print(1 - (h_new.GetBinContent(7) / h_new.GetBinContent(6)))			
 		h_stack.Add(h_new)
-		print(h_new.GetMaximum())
+		#print(h_new.GetMaximum())
 		legend.AddEntry(h_new, f)
 	c1 = ROOT.TCanvas()
 	h_stack.Draw('HIST NOSTACK PLC')
@@ -46,4 +51,4 @@ for param in param_list:
 		h_stack.SetMaximum(10**8)
 	legend.Draw()
 	#h_stack.Draw('HIST')
-	c1.SaveAs('plots/QCDOverlaid/{}.png'.format(param))
+	c1.SaveAs('plots/QCDOverlaid/{}/{}.png'.format(bTags, param))
