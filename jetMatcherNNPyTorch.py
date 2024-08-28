9f089328bd9e79df4c11f57128a48f4141687359
Devin Mahon <devinmahon@comcast.net>
2024-08-28 18:52:04 -0500

    Adding command-line arguments, adding sample weight functionality, automating network construction, improving training stats calculation

--- jetMatcherNNPyTorch.py
+++ jetMatcherNNPyTorch.py
@@ -3,6 +3,8 @@ import uproot
 import numpy as np
 import pandas as pd
 import pickle as pkl
+from datetime import datetime
+import argparse
 
 import torch
 from torch.utils.data import TensorDataset, DataLoader
@@ -24,15 +26,27 @@ from sklearn.metrics import RocCurveDisplay
 
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
+timeTag = datetime.now().strftime("%y-%m-%d-%H-%M")
+
 seed = 7
 np.random.seed(seed)
 
-outputTag = '24-07-26'
+parser = argparse.ArgumentParser(description='Jet Matcher NN PyTorch')
+parser.add_argument('--input',type=str,required=True,help='Path to input file')
+parser.add_argument('--tag',type=str,default='test',help='Tag for output label')
+parser.add_argument('--test',action='store_true',default=False,help='Do not save outputs')
+args = parser.parse_args()
 
-outputNNDir =    'output/jetMatcherNNPyTorch/{}'.format(outputTag)
-outputPlotsDir = 'plots/jetMatcherNNPyTorch/{}'.format(outputTag)
-if not os.path.exists(outputNNDir): os.makedirs(outputNNDir)
-if not os.path.exists(outputPlotsDir): os.makedirs(outputPlotsDir)
+# Input
+if not os.path.exists(args.input): 
+  print('ERROR: Provided input file does not exist. Exiting...')
+  quit()
+
+# Output
+if not args.test:
+  if args.tag == 'test': print('INFO: \"test\" will be used as the output tag')
+  outputDir = 'output/jetMatcherNNPyTorch/{}_{}'.format(args.tag,timeTag)
+  if not os.path.exists(outputDir): os.makedirs(outputDir)
 
 ##############################
 # LOAD & PREPARE DATA
@@ -40,17 +54,30 @@ if not os.path.exists(outputPlotsDir): os.makedirs(outputPlotsDir)
 
 batch_size = 50
 
-df = pd.read_csv('output/exportJetInfo/jets_1500_900.csv')
+df = pd.read_csv(args.input)
 
-inputs = df.values[:,0:-3]
+inputs = df.drop(['mStop','mChargino','isStopMatched','isChiMatched','isOther','jetBScore'],axis=1).values
+#inputs = df.values[:,2:-3] # 0-1: masses, last 3: truth
 nInputs = inputs.shape[1]
-targets = df.values[:,-3:]
+#targets = df.values[:,-3:]
+targets = df[['isStopMatched','isChiMatched','isOther']].values
 
-X_train, X_test, Y_train, Y_test = train_test_split(inputs,targets,test_size=0.1,random_state=seed)
-X_train, X_val, Y_train, Y_val   = train_test_split(X_train,Y_train,test_size=0.111,random_state=seed)
+# Number of nodes in hidden layer = average of input and output nodes
+nNodesHidden = np.floor((nInputs+3)/2).astype(int)
+
+# Set weights to balance different signal points
+df['mStop'] = df['mStop'].astype(str)
+df['mChargino'] = df['mChargino'].astype(str)
+df['mStop_mChargino'] = df[['mStop','mChargino']].agg('_'.join,axis=1)
+weightVals = 1 / df['mStop_mChargino'].value_counts(normalize=True)
+weights = df['mStop_mChargino'].map(weightVals)
+
+X_train, X_test, Y_train, Y_test, W_train, W_test   = train_test_split(inputs,targets,weights,test_size=0.1,random_state=seed,stratify=targets)
+X_train, X_val, Y_train, Y_val, W_train, W_val      = train_test_split(X_train,Y_train,W_train,test_size=0.111,random_state=seed,stratify=Y_train)
 
 scaler = StandardScaler().fit(X_train)
-with open('{}/scaler.pkl'.format(outputNNDir),'wb') as f: pkl.dump(scaler,f)
+if not args.test: 
+  with open('{}/scaler.pkl'.format(outputDir),'wb') as f: pkl.dump(scaler,f)
 
 X_train = scaler.transform(X_train)
 X_test = scaler.transform(X_test)
@@ -58,17 +85,20 @@ X_val = scaler.transform(X_val)
 
 tXTrain = torch.Tensor(X_train)
 tYTrain = torch.Tensor(Y_train)#.type(torch.LongTensor)
-datasetTrain = TensorDataset(tXTrain,tYTrain)
+tWTrain = torch.Tensor(W_train.to_numpy())
+datasetTrain = TensorDataset(tXTrain,tYTrain,tWTrain)
 loaderTrain = DataLoader(datasetTrain,batch_size=batch_size)
 
 tXTest = torch.Tensor(X_test)
 tYTest = torch.Tensor(Y_test)#.type(torch.LongTensor)
-datasetTest = TensorDataset(tXTest,tYTest)
+tWTest = torch.Tensor(W_test.to_numpy())
+datasetTest = TensorDataset(tXTest,tYTest,tWTest)
 loaderTest = DataLoader(datasetTest,batch_size=100)
 
 tXVal = torch.Tensor(X_val)
 tYVal = torch.Tensor(Y_val)#.type(torch.LongTensor)
-datasetVal = TensorDataset(tXVal,tYVal)
+tWVal = torch.Tensor(W_val.to_numpy())
+datasetVal = TensorDataset(tXVal,tYVal,tWVal)
 loaderVal = DataLoader(datasetVal,batch_size=100)
 
 ##############################
@@ -80,13 +110,14 @@ learningRate = 0.001
 class Net(nn.Module):
   def __init__(self):
     super(Net,self).__init__()
-    self.fc1 = nn.Linear(nInputs,nInputs)
-    self.fc2 = nn.Linear(nInputs,3)
+    self.fc1 = nn.Linear(nInputs,nNodesHidden)
+    self.fc2 = nn.Linear(nNodesHidden,3)
   def forward(self,x):
     x = F.relu(self.fc1(x))
     x = F.softmax(self.fc2(x),dim=1)
     return x
 
+torch.manual_seed(seed)
 network = Net()
 optimizer = optim.Adam(network.parameters(),lr=learningRate)
 
@@ -98,48 +129,49 @@ summary(network)
 
 nEpochs = 50
 
-#trainLosses = []
-#trainCounter = []
-#testLosses = []
-#testCounter = [i*len(loaderTrain.dataset) for i in range(nEpochs + 1)]
+lossFunction = nn.CrossEntropyLoss(reduction='none')
 
-lossFunction = nn.CrossEntropyLoss()
+def weightedLoss(output,target,weight):
+  weight = 1
+  return (lossFunction(output,target) * weight).mean()
 
 def train(epoch,loaderTrain):
   network.train()
-  for iBatch,(data,target) in enumerate(loaderTrain):
+  lossSum = 0
+  nCorrect = 0
+  for iBatch,(data,target,weights) in enumerate(loaderTrain):
     optimizer.zero_grad()
     output = network(data)
-    loss = lossFunction(output,target)
+    #loss = lossFunction(output,target)
+    loss = weightedLoss(output,target,weights)
+    lossSum += loss
     pred = output.data.max(1,keepdim=True)[1]
     target = target.data.max(1,keepdim=True)[1]
-    nCorrect = pred.eq(target.data.view_as(pred)).sum()
-    acc = nCorrect / len(data)
+    nCorrect += pred.eq(target.data.view_as(pred)).sum()
     loss.backward()
     optimizer.step()
-    if iBatch % 20 == 0:
-      print('Epoch {}: [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch,iBatch * len(data),len(loaderTrain.dataset),100. * iBatch / len(loaderTrain),loss.item()))
-      #trainLosses.append(loss.item())
-      #trainCounter.append(iBatch * len(data) + ((epoch) * len(loaderTrain.dataset) / nEpochs))
-  return loss.item(),acc.item()
+    if iBatch % 200 == 0:
+      print('Epoch {}: [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch+1,iBatch * len(data),len(loaderTrain.dataset),100. * iBatch / len(loaderTrain),loss.item()))
+  lossAvg = lossSum / len(loaderTrain)
+  acc = nCorrect / len(loaderTrain.dataset)
+  return lossAvg.item(),acc.item()
 
 def test(loaderTest):
   network.eval()
-  lossTest = 0
+  loss = 0
   nCorrect = 0
   with torch.no_grad():
-    for data,target in loaderTest:
+    for (data,target,weights) in loaderTest:
       output = network(data)
-      lossFunctionTest = nn.CrossEntropyLoss(reduction='sum')
-      lossTest += lossFunctionTest(output,target).item()
+      #lossFunctionTest = nn.CrossEntropyLoss(reduction='sum')
+      #loss += lossFunctionTest(output,target).item()
+      loss += weightedLoss(output,target,weights)
       pred = output.data.max(1,keepdim=True)[1]
       target = target.data.max(1,keepdim=True)[1]
       nCorrect += pred.eq(target.data.view_as(pred)).sum()
-  lossTest /= len(loaderTest.dataset)
+  loss /= len(loaderTest)
   acc = nCorrect / len(loaderTest.dataset)
-  #testLosses.append(lossTest)
-  print('Test: Average loss = {:.4f}, Accuracy = {}/{} ({:.0f}%)'.format(lossTest,nCorrect,len(loaderTest.dataset),100. * acc))
-  return lossTest,acc
+  return loss,acc
 
 def evaluate(loaderVal):
   network.eval()
@@ -148,19 +180,18 @@ def evaluate(loaderVal):
   outputs = []
   targets = []
   with torch.no_grad():
-    for data,target in loaderTest:
+    for (data,target,weights) in loaderTest:
       targets.extend(target.data.tolist())
       output = network(data)
-      lossFunctionTest = nn.CrossEntropyLoss(reduction='sum')
-      lossTest += lossFunctionTest(output,target).item()
+      #lossFunctionTest = nn.CrossEntropyLoss(reduction='sum')
+      #lossTest += lossFunctionTest(output,target).item()
+      lossTest += weightedLoss(output,target,weights)
       pred = output.data.max(1,keepdim=True)[1]
       target = target.data.max(1,keepdim=True)[1]
       nCorrect += pred.eq(target.data.view_as(pred)).sum()
       outputs.extend(output.data.tolist())
-  lossTest /= len(loaderTest.dataset)
+  lossTest /= len(loaderTest)
   acc = nCorrect / len(loaderTest.dataset)
-  #testLosses.append(lossTest)
-  print('Test: Average loss = {:.4f}, Accuracy = {}/{} ({:.0f}%)'.format(lossTest,nCorrect,len(loaderTest.dataset),100. * acc))
   return lossTest,acc,outputs,targets
 
 ##############################
@@ -176,17 +207,19 @@ for epoch in range(nEpochs):
   loss,acc = train(epoch,loaderTrain)
   lossesTrain.append(loss)
   accTrain.append(acc)
+  print('----- Epoch {} Train/Test Stats -----'.format(epoch+1))
+  print('Train:\tAverage loss = {:.5f},\tAccuracy = {:.1f}%'.format(loss,acc * 100.))
   loss,acc = test(loaderTest)
   lossesTest.append(loss)
   accTest.append(acc)
+  print('Test:\tAverage loss = {:.5f},\tAccuracy = {:.1f}%'.format(loss,acc * 100.))
+  print('-------------------------------------{}'.format('-' if epoch >= 10 else ''))
 print('Training complete. Running validation...')
 loss,acc,outputs,targets = evaluate(loaderVal)
-#print(trainCounter)
-#print(lossesTrain)
-#print(testCounter)
-#print(lossesTest)
 
-torch.save(network,'{}/jetMatcherNN.pt'.format(outputNNDir))
+if not args.test: 
+  print('Saving model to {}'.format(outputDir))
+  torch.save(network,'{}/jetMatcherNN.pt'.format(outputDir))
 
 plt.figure(figsize=(15,10))
 
@@ -194,6 +227,7 @@ rocLabels = ['Stop vs. Rest','Chargino vs. Rest','Other vs. Rest']
 for i in [0,1,2]:
   fpr, tpr, thresholds = roc_curve(np.array(targets)[:,i],np.array(outputs)[:,i])
   roc_auc = auc(fpr, tpr)
+  print('{} AUC = {}'.format(rocLabels[i],roc_auc))
   rocDisplay = RocCurveDisplay(fpr=fpr,tpr=tpr,roc_auc=roc_auc,estimator_name=rocLabels[i])
   #RocCurveDisplay.from_predictions(Y_test_onehot[:,0],Y_pred,name='stop vs. rest',color='red')
   ax = plt.subplot(2,2,3)
@@ -220,4 +254,4 @@ ax.legend(loc='lower right')
 ax.set_xlabel('Epoch')
 ax.set_ylabel('Accuracy')
 
-plt.savefig('{}/performance.pdf'.format(outputPlotsDir))
+if not args.test: plt.savefig('{}/performance.pdf'.format(outputDir))
